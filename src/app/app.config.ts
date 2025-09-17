5dfb7d60acd082626b432f72f9fb80b8a669d1b7
joaquin-fernandez <fjoaquin280@gmail.com>
2025-09-17 09:19:03 -0300

    actualizacion1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
