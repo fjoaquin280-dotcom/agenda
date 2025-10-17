984eb021feee50380baac0b0b7ed5f8bc68fe109
joaquin-fernandez <fjoaquin280@gmail.com>
2025-10-17 20:42:31 -0300

    creacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { ContactsPage } from './pages/contacts-page/contacts-page';
+import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginPage
+    },
+    {
+        path: "",
+        component: ContactsPage
+    },
+    {
+        path: "contacts/:id",
+        component: ContactDetailsPage
+    },
+];
---
+++ src/app/pages/contact-details-pages/contact-details-pages.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-details-pages',
+  imports: [],
+  templateUrl: './contact-details-pages.html',
+  styleUrl: './contact-details-pages.css'
+})
+export class ContactDetailsPages {
+
+}
